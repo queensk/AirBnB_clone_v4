1dd2c9664dbf9184085d8478ea7c245d0654605a
queensk <queenskisivuli@gmail.com>
2023-02-25 13:59:11 +0300

    Select some Amenities to be comfortable js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  let idDict = {};
+  $("input:checkbox").change(function () {
+    if ($(this).is(":checked")) {
+      idDict[$(this).attr("data-name")] = $(this).attr("data-id");
+    } else {
+      delete idDict[$(this).attr("data-name")];
+    }
+    let list = [];
+    $.each(idDict, function (index, value) {
+      list.push(index);
+    });
+    if (list.length === 0) {
+      $(".amenities h4").html("&nbsp;");
+    } else {
+      $(".amenities h4").text(list.join(", "));
+    }
+  });
+});
